a878e94c5b7ba0b23d3eb1ff99104ae7c7dddd98
murtuzaalisurti <murtuza.creativity@gmail.com>
2025-05-01 22:07:10 +0530

    feat: expire jwt in 1 day

--- backend/lib/jwt.ts
+++ backend/lib/jwt.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET;
-const JWT_EXPIRES_IN_STRING = process.env.JWT_EXPIRES_IN || '3600'; 
+const JWT_EXPIRES_IN_STRING = process.env.JWT_EXPIRES_IN || '1d'; 
 
 if (!JWT_SECRET) {
     throw new Error("JWT_SECRET environment variable is not set.");
